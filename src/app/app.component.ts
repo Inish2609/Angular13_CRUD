97e2faf66067cf06a996d0b5d87c7d0c77565a21
Inish2609 <inishraj.b.s003@gmail.com>
2024-03-19 11:23:23 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Angular_CRUD_With_JSON_Server_And_Material_UI';
+}
